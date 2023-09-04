48a70d8a8ba889792c67689785811344c920d4fd
Maxim Nikolaev <darwinnnn27@gmail.com>
2023-09-04 19:39:22 +0300

    switched to same-site: none

--- controllers/users.js
+++ controllers/users.js
@@ -64,6 +64,7 @@ const login = (req, res, next) => {
               maxAge: 3600000,
               httpOnly: true,
               sameSite: 'none',
+              secure: true,
             })
             .send(userInfo);
         });
